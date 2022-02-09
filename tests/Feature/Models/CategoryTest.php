cd2af2e32a0a43a12968f02ccfa3039682acf8f9
Paulo <paulo@btoolsb.com>
2022-02-09 20:16:16 -0300

    Create env for testing - New database create for test - Create feture test Category

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testList()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+    }
+
+    public function testAttributes()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $expectedCategoryKey = [
+            'id',
+            'name',
+            'description',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ];
+        $categoryKey = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing($expectedCategoryKey, $categoryKey);
+    }
+}
