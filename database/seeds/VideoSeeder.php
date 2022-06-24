9ab7f44885fdb069ac55dede8e7fd16d2e3fe587
paulori22 <paulinhoboneti@yahoo.com.br>
2022-06-24 16:40:26 -0300

    Category Resource - Created CategoryResource - Update BasicCrudController to use resource on the return data - Update Video Seeder with relations and files - Update CategoryControllerTest to reflect changes with the resource - New trait TestResource

--- database/seeds/VideoSeeder.php
+++ database/seeds/VideoSeeder.php
@@ -2,10 +2,18 @@
 
 use App\Models\Genre;
 use App\Models\Video;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 
 class VideoSeeder extends Seeder
 {
+    private $allGenres;
+    private $relations = [
+        'genres_id' => [],
+        'categories_id' => [],
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -13,18 +21,56 @@ class VideoSeeder extends Seeder
      */
     public function run()
     {
-        $genres = Genre::all();
+        $dir = \Storage::getDriver()->getAdapter()->getPathPrefix();
+        \File::deleteDirectory($dir, true);
+        $this->allGenres = Genre::all();
+        Model::reguard(); // active mass assignment on seed
         factory(Video::class, 100)
-            ->create()
-            ->each(function (Video $video) use ($genres) {
-                $subGenres = $genres->random(5)->load('categories');
-                $categoriesId = [];
-                foreach ($subGenres as $genre) {
-                    array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
-                }
-                $categoriesId = array_unique($categoriesId);
-                $video->categories()->attach($categoriesId);
-                $video->genres()->attach($subGenres->pluck('id')->toArray());
+            ->make()
+            ->each(function (Video $video) {
+                $this->fetchRelations();
+                Video::create(
+                    array_merge(
+                        $video->toArray(),
+                        [
+                            'video_file' => $this->getVideoFile(),
+                            'thumb_file' => $this->getImageFile(),
+                            'banner_file' => $this->getImageFile(),
+                            'trailer_file' => $this->getVideoFile(),
+                        ],
+                        $this->relations
+                    )
+                );
             });
+        Model::unguard();
+    }
+
+    public function fetchRelations()
+    {
+        $subGenres = $this->allGenres->random(5)->load('categories');
+        $categoriesId = [];
+        foreach ($subGenres as $genre) {
+            array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
+        }
+        $categoriesId = array_unique($categoriesId);
+        $genresId = $subGenres->pluck('id')->toArray();
+        $this->relations['categories_id'] = $categoriesId;
+        $this->relations['genres_id'] = $genresId;
+    }
+
+    public function getImageFile()
+    {
+        return new UploadedFile(
+            storage_path('faker/thumbs/atlantis_nexus_nebula.jpg'),
+            'atlantis_nexus_nebula.jpg'
+        );
+    }
+
+    public function getVideoFile()
+    {
+        return new UploadedFile(
+            storage_path('faker/videos/Doom_Eternal.mp4'),
+            'Doom_Eternal.mp4'
+        );
     }
 }
